3efb5399e1bd5ae7036a75709e7f794d2f55b0e1
akykaan <kaanaky77@gmail.com>
2021-03-20 00:57:56 +0300

    RecapProjectFrontEnd

---
+++ src/app/models/rental.ts
@@ -0,0 +1,10 @@
+export interface Rental{
+    id:number
+    carName:string
+    firstName:string
+    lastName:string
+    customerName:string
+    carId:number
+    rentDate:number
+    returnDate:number
+}
